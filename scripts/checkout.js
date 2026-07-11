9b780520820295a23576fd6113d17d0b5480401a
Jashwanth-0624 <rjashwanth562@gamil.com>
2026-07-11 21:16:43 +0530

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSumm} from './checkout/orderSumm.js';
 import {renderPaymentSumm} from './checkout/paymentSumm.js';
+import '../data/cart-oop.js';
 
 renderOrderSumm();
 renderPaymentSumm();
