b6eabdc2abe3056737b2c10a9eb2fba514b3e07c
Jashwanth-0624 <rjashwanth562@gamil.com>
2026-07-12 19:02:27 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import {renderOrderSumm} from './checkout/orderSumm.js';
 import {renderPaymentSumm} from './checkout/paymentSumm.js';
-import '../data/backend-practice.js';
+import {loadProducts} from '../data/products.js';
+// import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
-renderOrderSumm();
-renderPaymentSumm();
+loadProducts(() => {
+  renderOrderSumm();
+  renderPaymentSumm();
+});
