e56e7516b041c5406c070a4997fe9d5e4332d13e
Jashwanth-0624 <rjashwanth562@gamil.com>
2026-07-12 19:08:16 +0530

    Load products in beforeall hook

--- tests/checkout/orderSummTest.js
+++ tests/checkout/orderSummTest.js
@@ -1,11 +1,19 @@
 import {renderOrderSumm} from '../../scripts/checkout/orderSumm.js';
 import {loadfromstorage, cart} from '../../data/cart.js';
+import {loadProducts} from '../../data/products.js';
 
 describe('test suite : renderOrderSumm', () => {
       const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
       const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
-  beforeEach(() => {
-    spyOn(localStorage,'setItem');
+
+      beforeAll((done) => {
+        loadProducts(() => {
+          done();
+        });
+      });
+
+      beforeEach(() => {
+          spyOn(localStorage,'setItem');
 
           document.querySelector('.js-tests-container').innerHTML = `
             <div class="js-order-summary"></div>
