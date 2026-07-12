7a4d5af5785d8f87818bf1f2c4fb602d9c81a5ae
Jashwanth-0624 <rjashwanth562@gamil.com>
2026-07-12 14:58:03 +0530

    convert Products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderOrderSumm} from './checkout/orderSumm.js';
 import {renderPaymentSumm} from './checkout/paymentSumm.js';
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSumm();
 renderPaymentSumm();
