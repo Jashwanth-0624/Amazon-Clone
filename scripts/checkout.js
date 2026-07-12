173239308775c234d666552c1f3e7c4285821932
Jashwanth-0624 <rjashwanth562@gamil.com>
2026-07-12 14:28:15 +0530

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderOrderSumm} from './checkout/orderSumm.js';
 import {renderPaymentSumm} from './checkout/paymentSumm.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSumm();
 renderPaymentSumm();
