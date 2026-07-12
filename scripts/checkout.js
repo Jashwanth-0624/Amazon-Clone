e998a09b93d6a76baee4ae34a56d290277300bb4
Jashwanth-0624 <rjashwanth562@gamil.com>
2026-07-12 20:35:19 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,48 @@
 import {renderOrderSumm} from './checkout/orderSumm.js';
 import {renderPaymentSumm} from './checkout/paymentSumm.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js'
 // import '../data/backend-practice.js';
 //import '../data/cart-class.js';
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+      loadProducts(() => {
+        resolve('value1');
+      });
+  }),
+    new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    })
+
+]).then((values) => {
+  console.log(values)
   renderOrderSumm();
   renderPaymentSumm();
 });
+
+/*
+new Promise((resolve) => {
+      loadProducts(() => {
+        resolve('value');
+      });
+    })
+.then((value) =>{
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSumm();
+  renderPaymentSumm();
+});*/
+
+/*
+loadProducts(() => {
+  renderOrderSumm();
+  renderPaymentSumm();
+});*/
