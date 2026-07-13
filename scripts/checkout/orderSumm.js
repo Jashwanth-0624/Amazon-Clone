c3b74cd333edf89600579ca5d912fc92a8cb7362
Jashwanth-0624 <rjashwanth562@gamil.com>
2026-07-13 10:41:12 +0530

    regenerate html for order summary

--- scripts/checkout/orderSumm.js
+++ scripts/checkout/orderSumm.js
@@ -126,10 +126,7 @@ export function renderOrderSumm(){
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(
-          `.js-cart-item-container-${productId}`
-        );
-        container.remove();
+        renderOrderSummary();
 
         renderPaymentSumm();
         
